fe341c3657681f707a9b0b29314443585ba3e735
Karan Middha <karanmiddha2299@gmail.com>
2020-07-17 12:30:57 +0530

    created form in sign & signup pages

--- Server/client/src/components/screens/UserSignup.js
+++ Server/client/src/components/screens/UserSignup.js
@@ -1,8 +1,27 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom'
 const UserSignup = ()=>{
     return(
-        <h1>UserSignup</h1>
+          <div className="mycard">
+        <div className="card auth-card input-field">
+            <h2>Getwork</h2>
+            <input type="text" 
+            placeholder="email"
+            />
+             <input type="text" 
+            placeholder="name"
+            />
+             <input type="password" 
+            placeholder="password"
+            />
+            <button className="btn waves-effect waves-light #64b5f6 blue darken-1" 
+            >SignUp
+            </button>
+            <h5>
+                    <Link to="/signin">have an account?</Link>
+                </h5>
+        </div>
+    </div>
     )
 }
 
